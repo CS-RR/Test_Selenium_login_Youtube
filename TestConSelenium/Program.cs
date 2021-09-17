49581b61022af1d2d61980c5d87ea27c33a0dae8
Mr Blue <creyes19@uan.edu.co>
2021-09-17 00:14:27 -0500

    Agregar archivos de proyecto.

---
+++ TestConSelenium/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestConSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                driver.Navigate().GoToUrl("https://www.youtube.com/");
+                driver.FindElement(By.Name("search_query")).SendKeys("Google");
+                driver.FindElement(By.Id("search-icon-legacy")).SendKeys(Keys.Enter);
+                IWebElement bottonlog = driver.FindElement(By.XPath("/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[3]/div[2]/ytd-button-renderer"));
+                bottonlog.Click();
+                driver.FindElement(By.Id("identifierId"));
+
+            }
+        }
+    }
+}
