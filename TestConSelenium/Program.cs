4c89680e345d6a8a68210bee26536c431a407900
Mr Blue <creyes19@uan.edu.co>
2021-09-17 22:08:47 -0500

    Continuación.

--- TestConSelenium/Program.cs
+++ TestConSelenium/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
@@ -15,10 +16,11 @@ namespace TestConSelenium
                 driver.Navigate().GoToUrl("https://www.youtube.com/");
                 driver.FindElement(By.Name("search_query")).SendKeys("Google");
                 driver.FindElement(By.Id("search-icon-legacy")).SendKeys(Keys.Enter);
-                IWebElement bottonlog = driver.FindElement(By.XPath("/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[3]/div[2]/ytd-button-renderer"));
-                bottonlog.Click();
-                driver.FindElement(By.Id("identifierId"));
-
+                IWebElement buttonlog = driver.FindElement(By.XPath("/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[3]/div[2]/ytd-button-renderer"));
+                buttonlog.Click();
+                driver.FindElement(By.Id("identifierId")).SendKeys("tucorreo@.com");
+                IWebElement buttonnext = driver.FindElement(By.Id("identifierNext"));
+                buttonnext.Click();
             }
         }
     }
